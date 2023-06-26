dc43aba1ac648ae0eff9db464764111eacad8bcb
Jan Chlouba <cboubik@gmail.com>
2023-06-26 18:43:53 +0100

    Delete ChangePasswordRegister.js

--- ChangePasswordRegister.js
+++
@@ -1,8 +0,0 @@
-function changePasswords() {
-  if (document.getElementById("password").value.length != 0) {
-    var hash = CryptoJS.SHA3(document.getElementById("password").value, {
-      outputLength: 512,
-    });
-    document.getElementById("password").value = hash;
-  }
-}
