5a1456df9befe3e9312486e893f8c24b9ae27739
Boubik <cboubik@gmail.com>
2025-01-08 15:01:45 +0100

    refactor registration page layout and improve password hashing logic

--- register.php
+++ register.php
@@ -3,49 +3,76 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Split calculator | Register</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg" href="./images/calculator_favicon.svg">
+    <title>Register | Split calculator</title>
     <script src="./js/changePasswordRegister.js"></script>
+    <script src="./js/sha3.js"></script>
+    <link rel="stylesheet" href="style/default.css">
 </head>
 
 <body>
+    <header>
+        <section class="navigation">
+            <div class="nav-container">
+                <div class="brand">
+                    <img src="./images/icons8-calculator.svg" alt="">
+                    <a href="index.php">Split-Calculator</a>
+                </div>
 
-    <?php
-    include "functions.php";
-    $conn = connect_db();
-    session_start();
-    if (isset($_POST["username"]) and !is_null($_POST["username"])) {
-        $user_is_taken = user_is_taken($conn, $_POST["username"]);
-    }
-    $password_is_not_same = false;
-    $short_password = false;
-    if (isset($_POST["password1"]) and !is_null($_POST["password1"])) {
-        if (strcasecmp($_POST["password1"], $_POST["password2"])) {
-            $password_is_not_same = true;
+                <nav>
+                    <div class="nav-mobile"><a id="navbar-toggle" href="#!"><span></span></a></div>
+                    <ul class="nav-list">
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <?php echo isset($navbarItems) ? $navbarItems : ''; ?>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    </header>
+    <script src="./js/navbar.js"></script>
+    <main>
+        <?php
+        include "functions.php";
+        $conn = connect_db();
+        session_start();
+        if (isset($_POST["username"]) and !is_null($_POST["username"])) {
+            $user_is_taken = user_is_taken($conn, $_POST["username"]);
         }
-        if (strlen($_POST["password1"]) < 6) {
-            $short_password = true;
+        $password_is_not_same = false;
+        $short_password = false;
+        if (isset($_POST["password1"]) and !is_null($_POST["password1"])) {
+            if (strcasecmp($_POST["password1"], $_POST["password2"])) {
+                $password_is_not_same = true;
+            }
+            if (strlen($_POST["password1"]) < 6) {
+                $short_password = true;
+            }
+            if (!$user_is_taken && !$password_is_not_same && !$short_password) {
+                register($conn, $_POST["username"], $_POST["password1"]);
+                $_SESSION["username"] = $_POST["username"];
+                $_SESSION["password"] = $_POST["password1"];
+                header("Location: index.php");
+            }
         }
-        if (!$user_is_taken && !$password_is_not_same && !$short_password) {
-            register($conn, $_POST["username"], $_POST["password1"]);
-            $_SESSION["username"] = $_POST["username"];
-            $_SESSION["password"] = $_POST["password1"];
-            header("Location: index.php");
-        }
-    }
-    include "template.php";
-    ?>
+        ?>
 
-    <?php
-    if (isset($user_is_taken) and $user_is_taken) {
-        echo '<script>alert("Username is already taken.")</script>';
-    }
-    if (isset($short_password) and $short_password) {
-        echo '<script>alert("Password need to be at least 6 characters.")</script>';
-    }
-    if (isset($password_is_not_same) and $password_is_not_same) {
-        echo '<script>alert("Passwords need to be same.")</script>';
-    }
-    echo '
+        <?php
+        if (isset($user_is_taken) and $user_is_taken) {
+            echo '<script>alert("Username is already taken.")</script>';
+        }
+        if (isset($short_password) and $short_password) {
+            echo '<script>alert("Password need to be at least 6 characters.")</script>';
+        }
+        if (isset($password_is_not_same) and $password_is_not_same) {
+            echo '<script>alert("Passwords need to be same.")</script>';
+        }
+        echo '
     <div class="container">
             <div class="center">
                 <h1>Register</h1>
@@ -75,7 +102,8 @@
                 </form>
             </div>
         </div>';
-    ?>
+        ?>
+    </main>
 </body>
 
 <script>
